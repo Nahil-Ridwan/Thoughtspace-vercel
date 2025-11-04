0a064c0089c321d32bd28102c4725795cd8e0759
Nahil-Ridwan <nahilpp@gmail.com>
2025-11-04 14:23:40 +0530

    VERCEL-02

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api" //DYNAMIC CHANGED FOR DEPLOYMENT
+const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api` : "/api" //DYNAMIC CHANGED FOR DEPLOYMENT
 const api = axios.create({
     baseURL: BASE_URL
 })
