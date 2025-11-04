a74bf757ad70ee6389a8bdfd7b63f743e1e5ef16
Nahil-Ridwan <nahilpp@gmail.com>
2025-11-04 15:02:01 +0530

    VERCEL-3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api` : "/api" //DYNAMIC CHANGED FOR DEPLOYMENT
+const BASE_URL = import.meta.env.MODE === "development" ? "https://thoughtspace-vercel.vercel.app/api" : "/api" //DYNAMIC CHANGED FOR DEPLOYMENT
 const api = axios.create({
     baseURL: BASE_URL
 })
